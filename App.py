1d47e9236a2bc2e1c5005c37129ede79252b3625
Bruna Leticia <bruna.letavlis@outlook.com>
2022-04-11 11:22:38 -0300

    adicionado while e try

--- App.py
+++ App.py
@@ -3,36 +3,44 @@ import Repositorio as repoP
 
 
 def main():
-    print("1. adicionar pessoa")
-    print("2. ver pessoas adicionadas")
-    print("3. atualizar pessoa")
-    print("4. remover pessoa")
-    print("5. sair")
-    opcao = int(input("escolha uma opçao: "))
+    while True:
+        print("1. adicionar pessoa")
+        print("2. ver pessoas adicionadas")
+        print("3. atualizar pessoa")
+        print("4. remover pessoa")
+        print("5. sair")
+        print()
+        try:
+            opcao = int(input("escolha uma opçao: "))
+            print()
 
-    if opcao == 1:
-        nome = input("Nome: ")
-        email = input("Email: ")
-        telefone = input("Telefone: ")
-        endereco = input("Endereco: ")
-        cpf = input("Cpf: ")
+            if opcao == 1:
+                nome = input("Nome: ")
+                email = input("Email: ")
+                telefone = input("Telefone: ")
+                endereco = input("Endereco: ")
+                cpf = input("Cpf: ")
 
-        repoP.adicionar_pessoa(nome, email, telefone, endereco, cpf)
-    elif opcao == 2:
-        repoP.mostrar_pessoa()
-    elif opcao == 3:
-        repoP.mostrar_pessoa()
-        id_pessoa = int(input("Digite o id da pessoa que deseja atualizar: "))
-        email = input("Email: ")
-        telefone = input("Telefone: ")
-        endereco = input("Endereco: ")
-        repoP.atualizar_pessoa(id_pessoa, email, telefone, endereco)
-    elif opcao == 4:
-        repoP.mostrar_pessoa()
-        id_pessoa = int(input("Digite o id da pessoa que deseja remover: "))
-        repoP.remover_pessoa(id_pessoa)
-    elif opcao == 5:
-        exit(0)
+                repoP.adicionar_pessoa(nome, email, telefone, endereco, cpf)
+            elif opcao == 2:
+                repoP.mostrar_pessoa()
+            elif opcao == 3:
+                repoP.mostrar_pessoa()
+                id_pessoa = int(input("Digite o id da pessoa que deseja atualizar: "))
+                email = input("Email: ")
+                telefone = input("Telefone: ")
+                endereco = input("Endereco: ")
+                repoP.atualizar_pessoa(id_pessoa, email, telefone, endereco)
+            elif opcao == 4:
+                repoP.mostrar_pessoa()
+                id_pessoa = int(input("Digite o id da pessoa que deseja remover: "))
+                repoP.remover_pessoa(id_pessoa)
+            elif opcao == 5:
+                exit(0)
+            else:
+                print("Opção inválida!")
+        except ValueError:
+            print("Opção inválida! Por favor insira um número")
 
 
 if __name__ == "__main__":
